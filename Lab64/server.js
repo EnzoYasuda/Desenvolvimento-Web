30a48c88510649499b96c29ded1f30dd45888f39
Enzo Eiki Yasuda <enzo.ey2003@gmail.com>
2024-10-01 23:39:19 -0300

    oi

--- Lab64/server.js
+++ Lab64/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+var formidable = require('formidable');
 
 
 app.set('view engine', 'ejs');
@@ -13,12 +14,30 @@ app.get("/sobre",function(req, res) {
     res.sendFile(__dirname + "/sobre.html");
 });
 
-app.get("/erro404", function(req, res) {
-    res.sendFile(__dirname + "/error.html");
-});
-
 app.use(function(req, res, next) {
     res.render(__dirname + "/404.ejs", { url: req.url});
+});
+
+app.get("/upload",function(req, res) {
+    if (req.url == '/upload') {
+        var form = new formidable.IncomingForm();
+        form.parse(req, function (err, fields, files) {
+        var oldpath = files.filetoupload.filepath;
+        var newpath = 'C:/Users/Your Name/' + files.filetoupload.originalFilename;
+        fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+        res.write('File uploaded and moved!');
+        res.end();
+    });
+});
+    } else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    return res.end();
+    }
 });
 
 
